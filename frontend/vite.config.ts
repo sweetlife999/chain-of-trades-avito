02e40d41846030bce1111e88a3b581a316edc8b6
deadOk <fomka15012007@yandex.ru>
2026-08-08 17:05:02 +0500

    fix: оптимизировал сборку и правильно собрал ее

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,51 +17,6 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          groups: [
-            {
-              name: "react-vendor",
-              test: /[\\/]node_modules[\\/](?:react|react-dom|scheduler)[\\/]/,
-              priority: 50,
-            },
-            {
-              name: "redux-vendor",
-              test: /[\\/]node_modules[\\/](?:@reduxjs[\\/]toolkit|react-redux|redux|redux-thunk|immer|reselect|use-sync-external-store)[\\/]/,
-              priority: 40,
-            },
-            {
-              name: "router-vendor",
-              test: /[\\/]node_modules[\\/](?:react-router|react-router-dom|@remix-run[\\/]router)[\\/]/,
-              priority: 40,
-            },
-            {
-              name: "form-vendor",
-              test: /[\\/]node_modules[\\/](?:@hookform[\\/]resolvers|react-hook-form)[\\/]/,
-              priority: 40,
-            },
-            {
-              name: "validation-vendor",
-              test: /[\\/]node_modules[\\/]zod[\\/]/,
-              priority: 40,
-            },
-            {
-              name: "query-vendor",
-              test: /[\\/]node_modules[\\/]@tanstack[\\/](?:react-query|query-core)[\\/]/,
-              priority: 40,
-            },
-            {
-              name: "vendor",
-              test: /[\\/]node_modules[\\/]/,
-              priority: 10,
-            },
-          ],
-        },
-      },
-    },
-  },
   server: {
     proxy: {
       "/api": {
